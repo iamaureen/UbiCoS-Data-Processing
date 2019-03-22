866d1d87d990f3e2b3d7087a3acee10706b2f269
iamaureen <ishrat.ahmed@asu.edu>
2019-03-22 18:22:48 -0400

    read data from database and write into excel file

---
+++ gm_data.py
@@ -0,0 +1,38 @@
+import ast
+from xlwt import Workbook
+
+with open('Data/gm_data.txt') as file:
+    s= file.read()
+file.close()
+
+data1 = s.replace("}", "}\n")
+data2 = data1.split("\n")
+
+wb = Workbook()
+sheet1 = wb.add_sheet('Sheet1')
+row = 0
+col = 0
+
+for i in data2:
+    try:
+        k = ast.literal_eval(i)
+        for key, value in k.items():
+            print(key, value)
+            sheet1.write(row, col, key)
+            col += 1
+            for j in value:
+                sheet1.write(row, col, j)
+                row += 1
+            col -= 1
+            row += 1
+    except SyntaxError:
+        print("")
+
+wb.save('Data/gm.xls')
+
+
+
+
+
+
+
